bdcc1ad869c5200a34cb223dffb73c4f11269ee8
Dennis Krohn <dennis.krohn@th-luebeck.de>
2019-02-05 13:08:15 +0100

    added comment

--- includes/Loop.hooks.php
+++ includes/Loop.hooks.php
@@ -32,6 +32,8 @@ class LoopHooks {
 		global $wgRightsText, $wgRightsUrl, $wgRightsIcon, $wgLanguageCode, $wgDefaultUserOptions;
 
 		$dbr = wfGetDB( DB_REPLICA );
+		# Check if table exists. SetupAfterCache hook fails if there is no loop_settings table.
+		# maintenance/update.php can't create loop_settings table if SetupAfterCache Hook fails, so this check is nescessary.
 		if ( $dbr->tableExists( 'loop_settings' ) ) {
 		
 			$res = $dbr->select(
