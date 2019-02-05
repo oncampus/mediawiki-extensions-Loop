b4d1a3f3b67742fe17d8b919f6dc3b2968ef9012
Kevin Berg <mail@kevinberg.de>
2019-02-05 13:01:09 +0100

    undo hook changes

--- includes/Loop.hooks.php
+++ includes/Loop.hooks.php
@@ -4,7 +4,7 @@ class LoopHooks {
 	/**
 	 * Catch the Request to perform custom action LoopEditMode and LoopRenderMode
 	 *
-	 * This is attached to the MediaWiki 'BeforeInitialize' hook.
+	 * This is attached to the MediaWiki 'onBeforeInitialize' hook.
 	 *
 	 * @param Title $title
 	 * @param Article $article
@@ -16,38 +16,6 @@ class LoopHooks {
 	public static function onBeforeInitialize( $title, $article = null, $output, $user, $request, $wiki ) {
 
 		Loop::handleLoopRequest( $output, $request, $user );
-
-		return true;
-	}
-
-	/**
-	 * Apply settings set on Special:LoopSettings
-	 *
-	 * This is attached to the MediaWiki 'SetupAfterCache' hook.
-	 *
-	 * @return true
-	 */
-	public static function onSetupAfterCache(  ) {
-
-		global $wgRightsText, $wgRightsUrl, $wgRightsIcon, $wgLanguageCode, $wgDefaultUserOptions;
-/*
-		$dbr = wfGetDB( DB_REPLICA );
-		$res = $dbr->select(
-			'loop_settings',
-			array( 'lset_id', 'lset_rightstext', 'lset_rightsurl', 'lset_rightsicon', 'lset_languagecode', 'lset_skinstyle' ),
-			array(),
-			__METHOD__,
-			array( 'ORDER BY' => 'lset_id DESC LIMIT 1' )
-		);
-		$row = $res->fetchObject();
-
-		if ( isset( $row->lset_id ) ) {
-			$wgRightsText = ( empty( $row->lset_rightstext ) ? $wgRightsText : $row->lset_rightstext );
-			$wgRightsUrl = ( empty( $row->lset_rightsurl ) ? $wgRightsUrl : $row->lset_rightsurl );
-			$wgRightsIcon = ( empty( $row->lset_rightsicon ) ? $wgRightsIcon : $row->lset_rightsicon  );
-			$wgLanguageCode = ( empty( $row->lset_languagecode ) ? $wgLanguageCode : $row->lset_languagecode );
-			$wgDefaultUserOptions['LoopSkinStyle'] = ( empty( $row->lset_skinstyle ) ? 'loop-common' : $row->lset_skinstyle );
-		}*/
 
 		return true;
 	}
