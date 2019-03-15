9b8910478ce690a8974977b888785e31aad414c1
Dennis Krohn <dennis.krohn@th-luebeck.de>
2019-03-15 14:08:43 +0100

    generate page with placeholder message for extra footer

--- schema/LoopUpdater.php
+++ schema/LoopUpdater.php
@@ -11,10 +11,18 @@ class LoopUpdater {
 	 */
 	public static function onLoadExtensionSchemaUpdates( DatabaseUpdater $updater ) {
 		
+		global $wgParserOptions;
+
 		$updater->addExtensionUpdate(array( 'addTable', 'loop_structure_items', dirname( __FILE__ ) . '/loop_structure_items.sql', true ));
 		$updater->addExtensionUpdate(array( 'addTable', 'loop_structure_properties', dirname( __FILE__ ) . '/loop_structure_properties.sql', true ) );
 		$updater->addExtensionUpdate(array( 'addTable', 'loop_settings', dirname( __FILE__ ) . '/loop_settings.sql', true ) );
-		
+
+		$extraFooterPage = WikiPage::factory( Title::newFromText( "MediaWiki:ExtraFooter" ));
+		if ($extraFooterPage) {
+			$extraFooterContent = new WikitextContent( wfMessage( 'loopsettings-extra-footer-placeholder' )->inContentLanguage()->text() );
+			$extraFooterPage->doEditContent( $extraFooterContent, '', EDIT_NEW, false, $user );
+		}
+
 		return true;
 	}
 	
