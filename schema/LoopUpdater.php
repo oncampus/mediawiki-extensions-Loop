077bab80f8b7209ff1deb7e82ae478059c550503
Dennis Krohn <dennis.krohn@th-luebeck.de>
2019-03-15 14:53:03 +0100

    changed page creation process

--- schema/LoopUpdater.php
+++ schema/LoopUpdater.php
@@ -11,17 +11,15 @@ class LoopUpdater {
 	 */
 	public static function onLoadExtensionSchemaUpdates( DatabaseUpdater $updater ) {
 		
-		global $wgParserOptions;
+		global $wgScriptPath;
 
 		$updater->addExtensionUpdate(array( 'addTable', 'loop_structure_items', dirname( __FILE__ ) . '/loop_structure_items.sql', true ));
 		$updater->addExtensionUpdate(array( 'addTable', 'loop_structure_properties', dirname( __FILE__ ) . '/loop_structure_properties.sql', true ) );
 		$updater->addExtensionUpdate(array( 'addTable', 'loop_settings', dirname( __FILE__ ) . '/loop_settings.sql', true ) );
 
-		$extraFooterPage = WikiPage::factory( Title::newFromText( "MediaWiki:ExtraFooter" ));
-		if ($extraFooterPage) {
-			$extraFooterContent = new WikitextContent( wfMessage( 'loopsettings-extra-footer-placeholder' )->inContentLanguage()->text() );
-			$extraFooterPage->doEditContent( $extraFooterContent, '', EDIT_NEW, false, $user );
-		}
+		$extraFooterPage = WikiPage::factory( Title::newFromText( "MediaWiki:ExtraFooter2" ));
+		$extraFooterContent = new WikitextContent( wfMessage( 'loopsettings-extra-footer-placeholder', $wgScriptPath )->inContentLanguage()->text() );
+		$extraFooterPage->doEditContent( $extraFooterContent, '', EDIT_NEW, false, $user );
 
 		return true;
 	}
