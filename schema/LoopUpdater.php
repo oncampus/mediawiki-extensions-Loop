f30de46111bc48f52c86625e3aca233e4765f6d3
Dennis Krohn <dennis.krohn@th-luebeck.de>
2019-03-18 13:51:35 +0100

    improved page creation on install

--- schema/LoopUpdater.php
+++ schema/LoopUpdater.php
@@ -17,9 +17,9 @@ class LoopUpdater {
 		$updater->addExtensionUpdate(array( 'addTable', 'loop_structure_properties', dirname( __FILE__ ) . '/loop_structure_properties.sql', true ) );
 		$updater->addExtensionUpdate(array( 'addTable', 'loop_settings', dirname( __FILE__ ) . '/loop_settings.sql', true ) );
 
-		$extraFooterPage = WikiPage::factory( Title::newFromText( "MediaWiki:ExtraFooter2" ));
+		$extraFooterPage = WikiPage::factory( Title::newFromText( "MediaWiki:ExtraFooter" ));
 		$extraFooterContent = new WikitextContent( wfMessage( 'loopsettings-extra-footer-placeholder', $wgScriptPath )->inContentLanguage()->text() );
-		$extraFooterPage->doEditContent( $extraFooterContent, '', EDIT_NEW, false, $user );
+		$extraFooterPage->doEditContent( $extraFooterContent, '', EDIT_NEW );
 
 		return true;
 	}
