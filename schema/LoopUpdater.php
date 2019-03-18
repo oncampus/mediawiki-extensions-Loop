e1424d0c612fbeb346a4b9b90a4344db067977a6
Dennis Krohn <dennis.krohn@th-luebeck.de>
2019-03-18 17:07:25 +0100

    page creation removed

--- schema/LoopUpdater.php
+++ schema/LoopUpdater.php
@@ -10,16 +10,10 @@ class LoopUpdater {
 	 * @return bool true
 	 */
 	public static function onLoadExtensionSchemaUpdates( DatabaseUpdater $updater ) {
-		
-		global $wgServer, $wgArticlePath;
 
 		$updater->addExtensionUpdate(array( 'addTable', 'loop_structure_items', dirname( __FILE__ ) . '/loop_structure_items.sql', true ));
 		$updater->addExtensionUpdate(array( 'addTable', 'loop_structure_properties', dirname( __FILE__ ) . '/loop_structure_properties.sql', true ) );
 		$updater->addExtensionUpdate(array( 'addTable', 'loop_settings', dirname( __FILE__ ) . '/loop_settings.sql', true ) );
-
-		$extraFooterPage = WikiPage::factory( Title::newFromText( "MediaWiki:ExtraFooter" ));
-		$extraFooterContent = new WikitextContent( wfMessage( 'loopsettings-extra-footer-placeholder', $wgServer.str_replace('$1', '', $wgArticlePath) )->inContentLanguage()->text() );
-		$extraFooterPage->doEditContent( $extraFooterContent, '', EDIT_NEW );
 
 		return true;
 	}
