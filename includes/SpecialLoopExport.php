65fc86bad99db68f6e187e0a4df71bd5d255e9fe
Krohn <krohnd@th-luebeck.de>
2019-04-09 16:42:59 +0200

    added audio export logging

--- includes/SpecialLoopExport.php
+++ includes/SpecialLoopExport.php
@@ -37,47 +37,65 @@ class SpecialLoopExport extends SpecialPage {
 			case 'xml':
 				if ($user->isAllowed( 'loop-export-xml' )) {
 					$export = new LoopExportXml($structure, $request);
+					$logEntry = new ManualLogEntry( 'loopexport', "xml");
 				}
 				break;
 			case 'pdf':
 				if ($user->isAllowed( 'loop-export-pdf' )) {
 					$export = new LoopExportPdf($structure);
+					$logEntry = new ManualLogEntry( 'loopexport', "pdf");
 				}
 				break;
 			case 'mp3':
 				if ($user->isAllowed( 'loop-export-mp3' )) {
 					$export = new LoopExportMp3($structure);
+					$logEntry = new ManualLogEntry( 'loopexport', "mp3");
 				}
 				break;
 			case 'html':
 				if ($user->isAllowed( 'loop-export-html' )) {
 					$export = new LoopExportHtml($structure, $context);
+					$logEntry = new ManualLogEntry( 'loopexport', "html");
 				}
 			break;
 			case 'epub':
 				if ($user->isAllowed( 'loop-export-epub' )) {
 					$export = new LoopExportEpub($structure);
+					$logEntry = new ManualLogEntry( 'loopexport', "epub");
 				}
 			break;
 			case 'pageaudio':
 				if ($user->isAllowed( 'loop-pageaudio' )) {
 					$export = new LoopExportPageMp3($structure, $request);
+					# page audio logging is moved to LoopMp3.php
 				}
 				break;
 		}
 		
 		if ( $export != false ) {
+			$logMsg = "";
 
 			if ( $export->getExistingExportFile() && $export->fileExtension != "mp3" ) {
 					$export->getExistingExportFile();
+					$logMsg = wfMessage("log-export-reused")->text();
+					
 			} else {
 				$export->generateExportContent();
-					
+				
 				if ( $export->exportDirectory != "/export/html" && $export->fileExtension != "mp3" ) { # don't cache html exports
 					$export->saveExportFile();
-				}
+					$logMsg = wfMessage("log-export-generated")->text();
+				} 
 			}
 			if ($export->getExportContent() != null ) {
+				
+				if ( isset($logEntry) ) {
+					$logEntry->setTarget( Title::newFromId(1) );
+					$logEntry->setPerformer( User::newFromId(0) ); 
+					$logEntry->setParameters( [ '4::paramname' => $logMsg ] );
+					$logid = $logEntry->insert();
+				}
+
 				$this->getOutput()->disable();
 				wfResetOutputBuffers();
 				$export->sendExportHeader();
