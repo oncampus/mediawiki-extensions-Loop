32aaf651c4674752b2cead4197b2c7dee7658da5
oncampus <education@oncampus.de>
2019-05-21 09:48:43 +0200

    Test

--- includes/LoopArea.php
+++ includes/LoopArea.php
@@ -3,76 +3,129 @@
 class LoopArea {
 
 	static function onParserInit( Parser $parser ) {
-		//global $wgOut;
 		$parser->setHook( 'loop_area', 'LoopArea::renderLoopArea' ); 
-		//$wgOut->addModules( 'ext.looparea' );
 		return true;
 	}
 	
+	public static $typeOptions = [
+		'learningobjectives',
+		'markedsentence',
+		'timerequirement',
+		'indentation',
+		'arrangement',
+		'definition',
+		'annotation',
+		'experiment',
+		'reflection',
+		'sourcecode',
+		'important',
+		'websource',
+		'exercise',
+		'citation',
+		'practice',
+		'question',
+		'practice',
+		'question',
+		'summary',
+		'example',
+		'formula',
+		'notice',
+		'norm',
+		'task',
+		'area',
+		'law'
+	];
+
+	public static $renderOptions = [
+		'marked',
+		'icon',
+		'none'
+	];
+
 	static function renderLoopArea( $input, array $args, Parser $parser, PPFrame $frame ) {
-		
-		//default
-		$argtype = 'area';
-		$iconimg = 'example';
-		
-		$allowed_attr = ['type', 'icontext', 'icon']; // allowed attributes for loop_area
+		try {
+			// if type attribute exists ...
+			if( isset( $args['type'] ) ) {
+				
+				// ... check if attribute contains allowed type
+				if( in_array( $args['type'], self::$typeOptions ) ) {
+					$argtype = $args['type'];
+					$iconimg = $argtype;
 
-		$arraydiff = array_diff(array_keys($args), $allowed_attr);
-
-		// todo: check if no attribute is defined
-
-
-		if($arraydiff) {
-			//$e = new LoopException( wfMessage( 'looparea-error-unknown-attribute' ));
-			//$this->getParser()->addTrackingCategory( 'loop-tracking-category-error' );
-			//$this->error = $e;
-			print_r("error"); //temp
-		}
-
-
-
-		if( !empty( $args['type'] ) ) {
-			$argtype = $args['type'];
+					// catch special cases
+					if ( $argtype === 'websource' ) { // because icon with this name does not exist, using 'link-external' icon
+						$iconimg = 'link-external';
+					} elseif ( $argtype === 'indentation' ) { // because icon with this name does not exist, using 'watch' icon
+						$iconimg = 'watch';
+					}
+				} else {	
+					throw new LoopException( wfMessage( 'looparea-error-unknown-type-attribute', $args['type'], implode( ', ', self::$typeOptions ) )->text() );
+				}
+			} else {
+				// ... set default type
+				$argtype = 'important';
+				$iconimg = $argtype;
+			}
+		} catch ( LoopException $e) {
+			$parser->addTrackingCategory( 'loop-tracking-category-error' );
+			$error = $e;
+			$argtype = 'important';
 			$iconimg = $argtype;
-		}
-
-		// catch special cases
-		if ( $argtype === 'websource' ) { // because icon with this name does not exist, using 'link-external' icon
-			$iconimg = 'link-external';
-		} elseif ( $argtype === 'indentation' ) { // because icon with this name does not exist, using 'watch' icon
-			$iconimg = 'watch';
 		}
 
 		// Determine how the box renders, 'rendermarked' is default
 		$cssrender = 'rendermarked';
 
-		if( array_key_exists( 'render', $args ) ) { // array_key_exists() because code convention forbids isset()
-			if ( $args['render'] === 'none' ) {
-				$cssrender = 'renderhide';
-			} elseif ( $args['render'] === 'icon') {
-				$cssrender = 'rendericon';
-			} elseif ( $args['render'] === 'marked') {
-				$cssrender = 'rendermarked';
+		try {
+			if( array_key_exists( 'render', $args ) ) { // array_key_exists() because code convention forbids isset()
+				if ( $args['render'] === 'none' ) {
+					$cssrender = 'renderhide';
+				} elseif ( $args['render'] === 'icon' ) {
+					$cssrender = 'rendericon';
+				} elseif ( $args['render'] === 'marked') {
+					$cssrender = 'rendermarked';
+				} else {
+					throw new LoopException( wfMessage( 'looparea-error-unknown-render-attribute', $args['render'], implode( ', ', self::$renderOptions ) )->text() );
+				}
 			}
+			
+		} catch ( LoopException $e) {
+			$parser->addTrackingCategory( 'loop-tracking-category-error' );
+			$error = $e;
 		}
 
-		$icontext = wfMessage( 'looparea-icon-text-'.$argtype );
+		$icontext = wfMessage( 'looparea-icon-text-' . $argtype )->text();
 
 		// Allow overwriting icon text when parameter 'icontext' is used
 		if( array_key_exists( 'icontext', $args ) ) { // array_key_exists() because code convention forbids isset()
-			$icontext = $args['icontext'];
+			$icontext = strtolower( $args['icontext'] );
 		}
-		
-		// Allow overwriting icon image when parameter 'icon' is used
+				
 		$cssicon = 'ic ic-' . $iconimg;
 		$ownicon = '';
+		
+		// Allow overwriting icon image when parameter 'icon' is used
 		if( array_key_exists( 'icon', $args ) ) { // array_key_exists() because code convention forbids isset()
-			$cssicon = 'ownicon';
-			$owniconurl = wfLocalFile( $args['icon'] )->getCanonicalURL();
-			$ownicon = 'style="background-image: url(' . $owniconurl . ')"'; 
+
+			try {
+				if( file_exists( wfLocalFile( $args['icon'] )->getLocalRefPath() ) ) {
+					$owniconurl = wfLocalFile( $args['icon'] )->getCanonicalURL();
+					$cssicon = 'ownicon';
+					$ownicon = 'style="background-image: url(' . $owniconurl . ')"'; 
+				} else {
+					throw new LoopException( wfMessage( 'looparea-error-imagenotfound', $args['icon'] )->text() );
+				}
+			} catch( Exception $e) {
+				$parser->addTrackingCategory( 'loop-tracking-category-error' );
+				$error = $e;
+			}
 		}
 
-		$ret = '<div class="looparea ' . $cssrender . ' looparea-'. $iconimg .'">';
+		$ret = '';
+		if ( isset( $error ) ) {
+			$ret .= $error;
+		} 
+		$ret .= '<div class="looparea ' . $cssrender . ' looparea-'. $iconimg .'">';
 		$ret .= '<div class="looparea-container mb-3">';
 		$ret .= '<div class="looparea-left pt-3 px-1">';
 		$ret .= '<span class="' . $cssicon . '" ' . $ownicon . '></span>';
