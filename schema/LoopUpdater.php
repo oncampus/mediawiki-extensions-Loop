635eb750a812206b66cd161e24cf8b6b5bc9ffa2
Krohn <krohnd@th-luebeck.de>
2019-06-14 13:50:36 +0200

    added migration

--- schema/LoopUpdater.php
+++ schema/LoopUpdater.php
@@ -17,9 +17,44 @@ class LoopUpdater {
 		$updater->addExtensionUpdate(array( 'addTable', 'loop_object_index', dirname( __FILE__ ) . '/loop_object_index.sql', true ) );
 		
 		Loop::setupLoopPages();
-
+		
+		$dbr = wfGetDB( DB_REPLICA );
+		if ( $dbr->tableExists( 'loop_object_index' ) ) { #sonst bricht der updater ab. updater muss so jetzt zweimal laufen #todo
+		
+			self::saveAllWikiPages();
+		}
 		return true;
 	}
-	
+
+	private static function saveAllWikiPages() {
+		
+		global $wgOut;
+		$dbr = wfGetDB( DB_REPLICA );
+		$res = $dbr->select(
+			array(
+				'page'
+			),
+			array(
+				'page_id',
+				'page_namespace'
+			),
+			array(
+				'page_namespace = 0'
+			),
+			__METHOD__
+		);
+		
+		foreach( $res as $row ) {
+			#todo $wgparser
+			$tmpFPage = new FlaggableWikiPage ( Title::newFromId( $row->page_id, NS_MAIN ) );
+			$stableRev = $tmpFPage->getStable();
+			if ( $stableRev == 0 ) {
+				$stableRev = $tmpFPage->getRevision()->getId();
+			}
+			$tmpFPage->doEditContent( $tmpFPage->getContent(), '', EDIT_FORCE_BOT, $stableRev, $wgOut->getUser() );
+			#$tmpPage = WikiPage::factory( Title::newFromId( $row->page_id, NS_MAIN ));
+			#$tmpPage->doEditContent( $tmpPage->getContent(), '', EDIT_FORCE_BOT, $tmpPage->getRevision()->getId(), $wgOut->getUser() );
+		}
+	}
 }
 ?>
--- schema/loop_structure_items.sql
+++ schema/loop_structure_items.sql
@@ -13,3 +13,12 @@ CREATE TABLE IF NOT EXISTS /*_*/loop_structure_items (
 	PRIMARY KEY (lsi_id),
 	UNIQUE KEY structure_article (lsi_structure,lsi_article)
 )/*$wgDBTableOptions*/;
+
+-- Migrate old structures items
+INSERT INTO /*$wgDBprefix*/loop_structure_items (lsi_structure,lsi_article,lsi_previous_article,lsi_next_article,lsi_parent_article,lsi_toc_level,lsi_sequence,lsi_toc_number,lsi_toc_text) SELECT IndexArticleId, ArticleId, PreviousArticleId, NextArticleId, ParentArticleId, TocLevel, Sequence, TocNumber, TocText FROM /*$wgDBprefix*/loopstructure;
+
+-- Migate toc page in new namespace
+UPDATE page SET page_namespace = 3100, page_title = (SELECT lsi_toc_text from loop_structure_items WHERE lsi_toc_level = 0) WHERE page_title = 'Loop_toc' AND page_namespace = 8;
+
+-- Delete old table
+--DROP TABLE IF EXISTS /*$wgDBprefix*/loopstructure; 
