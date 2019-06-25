f08e08a263f6722b059a307ad3f1dc9ff490957e
oncampus <education@oncampus.de>
2019-06-25 11:13:09 +0200

    minor fixes

--- includes/LoopSpoiler.php
+++ includes/LoopSpoiler.php
@@ -19,19 +19,19 @@ class LoopSpoiler {
 		'in_text_transparent'
 	);
 	
-	public function setId($id) {
+	public function setId( $id ) {
 		$this->mId = $id;
 	}
 
-	public function setType($type) {
+	public function setType( $type ) {
 		$this->mType = $type;
 	}
 	
-	public function setBtnText($btn_text) {
+	public function setBtnText( $btn_text ) {
 		$this->mBtnText = $btn_text;
 	}
 	
-	public function setContent($content) {
+	public function setContent( $content ) {
 		$this->mContent = $content;
 	}
 	
@@ -52,18 +52,16 @@ class LoopSpoiler {
 	}
 
 	public static function onParserSetup( Parser &$parser ) {
-		
-		$parser->setHook( 'spoiler', 'LoopSpoiler::renderLoopSpoiler2' );
-		$parser->setHook( 'loop_spoiler', 'LoopSpoiler::renderLoopSpoiler2' ); // entfernen?
+		$parser->setHook( 'spoiler', 'LoopSpoiler::renderLoopSpoiler' );
+		$parser->setHook( 'loop_spoiler', 'LoopSpoiler::renderLoopSpoiler' ); // behalten?
 		return true;
 	}
 
-	public static function renderLoopSpoiler2( $input, array $args, Parser $parser, PPFrame $frame ) {
-		
+	public static function renderLoopSpoiler( $input, array $args, Parser $parser, PPFrame $frame ) {
 		try {		
-			$spoiler = LoopSpoiler::newFromTag($input, $args, $parser, $frame);
-			$spoiler->setContent($parser->recursiveTagParse($input));
-			$return  = $spoiler->render2();
+			$spoiler = LoopSpoiler::newFromTag( $input, $args, $parser, $frame );
+			$spoiler->setContent( $parser->recursiveTagParse( $input ) );
+			$return  = $spoiler->render();
 		} catch ( LoopException $e ) {
 			$parser->addTrackingCategory( 'loop-tracking-category-loop-error' );
 			$return = "$e";
@@ -72,74 +70,20 @@ class LoopSpoiler {
 		return $return;
 	}
 	
-	
-	/**
-	 * Render
-	 */
-	public function render2() {
+	public function render() {
 		$content = $this->getContent();
 		while ( substr( $content, -1, 2 ) == "\n" ) { # remove newlines at the end of content for cleaner html output
 			$content = substr( $content, 0, -1 );
 		}
-		$return = '<span class="btn loopspoiler loopspoiler_type_'.$this->getType().'" onclick="$(\'#'.$this->getId().'\').toggleClass(\'d-inline\'); $(\'#'.$this->getId().'\').toggleClass(\'d-none\'); return false;" >'.$this->getBtnText().'</span>';
-		$return .= '<div id="'.$this->getId().'" class="loopspoiler_content_wrapper d-none loopspoiler_type_'.$this->getType().'">';
+		$return = '<span class="btn loopspoiler loopspoiler_type_'.$this->getType().'" onclick="$(\'#'.$this->getId().'\').toggle(); $(this).toggleClass(\'spoileractive\'); return false;" >'.$this->getBtnText().'</span>';
+		$return .= '<div id="'.$this->getId().'" class="loopspoiler_content_wrapper loopspoiler_type_'.$this->getType().'">';
 		$return .= '<div class="loopspoiler_content">'.$content.'</div>';
 		$return .= "\n</div>";
 		
 		return $return;
-		
-	}
-
-	public static function renderLoopSpoiler( $input, array $args, Parser $parser, PPFrame $frame ) {
-		try {		
-			$spoiler = LoopSpoiler::newFromTag($input, $args, $parser, $frame);
-			//$return = $spoiler->render($input, $parser, $spoiler);
-
-
-
-
-			$toset = $input;
-		
-			$toset = $parser->recursiveTagParse($toset); 
-			//$toset = $parser->killMarkers( $toset );
-			//$toset = $parser->insertStripItem($toset, $parser->mStripState);
-			#$toset = $parser->killMarkers( $toset ); #killmarkers muss für loopobjekte etc raus
-			$spoiler->setContent($toset);
-
-	
-/*			$html = '<span class="loopspoilercontainer loopspoilercontainer_'.$spoiler->getType().'">';
-			$html .= '<span class="btn loopspoilerbutton loopspoiler_type_'.$spoiler->getType().'" onclick="$(\'#'.$spoiler->getId().'\').toggle(); $(this).toggleClass(\'spoileractive\'); $(this).parent().toggleClass(\'parentspoileractive\');return false;" >'.$spoiler->getBtnText().'</span>';
-			$html .= '<span id="'.$spoiler->getId().'" class="loopspoiler_type_'.$spoiler->getType().'" style="display:none;">';
-			$html .= '<span class="loopspoiler_content">'.$spoiler->getContent().'</span>'; 
-			$html .= '</span>';
-			$html .= '</span>';
-			$html .= '</span>';
-			*/
-
-			$html = Html::openElement('span', ['class'=>'loopspoilercontainer d-inline loopspoilercontainer_'.$spoiler->getType()]);
-			$html .= Html::element('span', ['class'=>'btn loopspoilerbutton loopspoiler_type_'. $spoiler->getType(), 'onclick'=>'$(\'#'.$spoiler->getId().'\').toggle(); $(this).toggleClass(\'spoileractive\'); $(this).parent().toggleClass(\'parentspoileractive\');return false;'], $spoiler->getBtnText());
-			$html .= Html::openElement('span', ['id'=>$spoiler->getId(), 'class'=>'loopspoiler_type_'.$spoiler->getType(), 'style'=>'display:none;']);
-			$html .= $spoiler->getContent(); # inhalte wurden als text angezeigt, wenn sie in einem html::element als content eingefügt wurden. (delete comment)
-			$html .= Html::closeElement('span');
-			$html .= Html::closeElement('span');
-			$return = $html;
-			#dd($toset,$input,$html);
-
-			//$return = $parser->killMarkers( $return );
-
-			//$return = $parser->insertStripItem($return, $parser->mStripState);
-			//$return = $parser->recursiveTagParse($return);
-			
-			//return [$parser->insertStripItem( $return, $parser->mStripState ), 'noparse'=>false];
-			return $return;
-		} catch ( LoopException $e ) {
-			$parser->addTrackingCategory( 'loop-tracking-category-loop-error' );
-			return $e;
-		}
 	}
 
 	public static function newFromTag( $input, array $args, Parser $parser, PPFrame $frame ) {
-	
 		global $wgLoopSpoilerDefaultType;
 		
 		$spoiler = new LoopSpoiler();
@@ -148,75 +92,27 @@ class LoopSpoiler {
 		
 		// set spoiler type to standard if not submitted.
 		if ( ! isset( $args['type'] ) ) {
-			$spoiler->setType($wgLoopSpoilerDefaultType); # muss noch in extension.json/config gesetzt werden oder einfach hier den standard definieren
+			if( $wgLoopSpoilerDefaultType == null ) {
+				$wgLoopSpoilerDefaultType = self::$mSpoilerTypes[0]; // default type: "default"
+			}
+			$spoiler->setType( $wgLoopSpoilerDefaultType ); # muss noch in extension.json/config gesetzt werden
 		} else {
 			$spoiler->setType( htmlspecialchars ( $args['type'] ) );
 		}		
 		
 		// throw exception if spoiler type is not valid
-		if ( !empty($spoiler->getType()) && !in_array ( $spoiler->getType(), self::$mSpoilerTypes ) ) {
-			throw new LoopException( wfMessage ( 'loopspoiler-error-unknown-spoilertype', $spoiler->getType(), implode ( ', ',self::$mSpoilerTypes ) ) );
+		if ( !empty( $spoiler->getType() ) && !in_array ( $spoiler->getType(), self::$mSpoilerTypes ) ) {
+			throw new LoopException( wfMessage ( 'loopspoiler-error-unknown-spoilertype', $spoiler->getType(), implode ( ', ', self::$mSpoilerTypes ) ) );
 		}		
 		
 		// button text
 		if ( !isset( $args['text'] ) ) {
-			$spoiler->setBtnText(wfMessage('loopspoiler-default-title')->inContentLanguage()->text());
+			$spoiler->setBtnText( wfMessage( 'loopspoiler-default-title' )->inContentLanguage()->text() );
 		} else {
-			$spoiler->setBtnText( $parser->recursiveTagParse(htmlspecialchars ( $args['text'] )) );
+			$spoiler->setBtnText( $parser->recursiveTagParse( htmlspecialchars ( $args['text'] ) ) );
 		}		
-
 
 		return $spoiler;
 	}
 		
-
-	
-
-/*
-
-NOT ACTIVE ATM
-
-*/
-
-/*
-	public function render($input, $parser, $spoiler) {
-
-		$toset = $input;
-		
-		$toset = $parser->recursiveTagParse($toset); 
-		$toset = $parser->killMarkers( $toset );
-		$toset = $parser->insertStripItem($toset, $parser->mStripState);
-
-		$spoiler->setContent($toset);
-*/
-
-
-		//dd($this->getContent());
-		/*
-			Attention: Using only spans because "return [$parser->insertStripItem( $html, $parser->mStripState ), 'noparse' =>true, 'isHTML'=>true];"
-			and variants of this snippet is somehow ignored or interpreted differently than it should. Already one single div inside this snippet breaks
-			the spoiler tag. Found no way to prevent p tags from generating before and/or after spoiler tag (inline spoilers) other than using inline tags
-		*/
-/*
-		$html = '<span class="loopspoilercontainer loopspoilercontainer_'.$this->getType().'">';
-		$html .= '<span class="btn loopspoilerbutton loopspoiler_type_'.$this->getType().'" onclick="$(\'#'.$this->getId().'\').toggle(); $(this).toggleClass(\'spoileractive\'); $(this).parent().toggleClass(\'parentspoileractive\');return false;" >'.$this->getBtnText().'</span>';
-		$html .= '<span id="'.$this->getId().'" class="loopspoiler_type_'.$this->getType().'" style="display:none;">';
-		$html .= '<span class="loopspoiler_content">'.$this->getContent().'</span>'; 
-		$html .= '</span>';
-		$html .= '</span>';
-		
-		$render = $html;*/
-		//$render = $html;
-		//$render = $parser->killMarkers($render);
-		//$render = $parser->insertStripItem($render, $parser->mStripState);
-		//dd($render);
-		//$text = $html;
-		//$striped_text = $parser->killMarkers( $render );
-		//$render = $parser->recursiveTagParse($striped_text);
-		//$render = $parser->insertStripItem($render, $parser->mStripState);		
-		//dd($render);
-
-	//	return $render;
-	//}
-
 }
