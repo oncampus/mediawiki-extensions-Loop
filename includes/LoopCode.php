5ffaa49d47ce90b22048a34522818c76666b2eef
Krohn <krohnd@th-luebeck.de>
2019-06-27 16:46:47 +0200

    added code parsing (preliminary)

---
+++ includes/LoopCode.php
@@ -0,0 +1,16 @@
+<?php class LoopCode {
+
+    static function onParserSetup( Parser $parser ) {
+        $parser->setHook( 'code', 'LoopCode::renderCodeTag' ); 
+        return true;
+    }
+
+    static function renderCodeTag( $input, array $args, Parser $parser, PPFrame $frame ) {
+        $content = str_replace('<', '&lt;', $input);
+        $content = str_replace('>','&gt;', $content);
+
+        $newcontent = '<code>'.$content.'</code>';
+        return $newcontent;
+    }
+
+}
