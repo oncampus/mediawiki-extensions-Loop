6aba24f603448df22c2a6d9960502cf7da5370eb
Krohn <krohnd@th-luebeck.de>
2019-07-08 15:07:46 +0200

    added glossary functions

--- includes/LoopGlossary.php
+++ includes/LoopGlossary.php
@@ -1,25 +1,38 @@
 <?php
-
+/**
+ * @author Dennis Krohn @krohnden
+ */
 if ( !defined( 'MEDIAWIKI' ) ) {
 	die( "This file cannot be run standalone.\n" );
 }
 
-
 use MediaWiki\MediaWikiServices;
 
 class LoopGlossary {
+	
+	public static function getShowGlossary() {
+		
+		$showGlossary = false;
 
-	public function hasObjects( $type ) {
-		$objects = LoopObjectIndex::getObjectsOfType ( $type );
-		$structureItems = $this->getStructureItems();
-
-		foreach ( $structureItems as $item ) {
-			if ( isset ( $objects[$item->article] ) ) {
-				return true;
-			}
+		$dbr = wfGetDB( DB_REPLICA );
+		$res = $dbr->select(
+			array(
+				'page'
+			),
+			array(
+				'page_id'
+			),
+			array(
+				'page_namespace = 3000'
+			),
+			__METHOD__
+		);
+		foreach ( $res as $row ) {
+			$showGlossary = true;
+			break;
 		}
 
-		return false;
+		return $showGlossary;
 	}
 	
 }
@@ -31,11 +44,14 @@ class SpecialLoopGlossary extends SpecialPage {
 	}
 
 	public function execute( $sub ) {
-		$user = $this->getUser();
-
+		
 		$linkRenderer = MediaWikiServices::getInstance()->getLinkRenderer();
+		$user = $this->getUser();
 		$out = $this->getOutput();
 
+		$out->setPageTitle(wfMessage('loop-glossary-title'));
+		$html = '<h1>' . wfMessage('loop-glossary-title') . '</h1>' ;
+		$glossaryItems = array();
 		
 		$dbr = wfGetDB( DB_REPLICA );
 		$res = $dbr->select(
@@ -44,20 +60,35 @@ class SpecialLoopGlossary extends SpecialPage {
 			),
 			array(
 				'page_id',
-				'page_namespace'
+				'page_title'
 			),
 			array(
 				'page_namespace = 3000'
 			),
 			__METHOD__
 		);
-		dd($res);
-
-
-		if ( $user->isAllowed('loop-edit-literature') ) {
-			
+		foreach ( $res as $row ) {
+			$glossaryItems[ $row->page_title ] = Title::newFromId ( $row->page_id );
 		}
-		$html = "Hello world!";
+		
+		if ( ! empty( $glossaryItems ) ) {
+			$html .= '<div class="list-group list-group-flush">';
+			sort( $glossaryItems );
+			foreach ( $glossaryItems as $pageTitle => $titleObject ) {
+
+				$html .= $linkRenderer->makeLink(
+					$titleObject,
+					$titleObject->mTextform,
+					array( 'class' => 'list-group-item list-group-item-action' )
+				);
+			}
+			$html .= '</div>';
+
+		} else {
+			$html .= wfMessage('loop-glossary-empty');
+		}
+
+		$out->addHTML( $html );
 
     }
 
