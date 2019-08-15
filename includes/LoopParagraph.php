dd9928524c0772a629a1186301c63bf1659f41a7
oncampus <education@oncampus.de>
2019-08-15 08:16:22 +0200

    minor adjustment

--- includes/LoopParagraph.php
+++ includes/LoopParagraph.php
@@ -15,7 +15,7 @@ class LoopParagraph {
 
     static function renderParagraph( $input, array $args, Parser $parser, PPFrame $frame ) {
 
-        // for now loop_paragraph supports citations
+        // for now loop_paragraph only supports citations
         $html = '<div class="loopparagraph">';
         $html .= '<div class="loopparagraph_left">';
         $html .= '<span class="ic ic-citation"></span>';
@@ -23,7 +23,11 @@ class LoopParagraph {
         $html .= '<div class="loopparagraph_right">';
         $html .= $parser->recursiveTagParseFully( $input );
         $html .= '</div>';
-        $html .= '<span class="loopparagraph_copyright">' . $args['copyright'] . '</span>';
+        
+        if( isset( $args['copyright'] ) ) {
+            $html .= '<span class="loopparagraph_copyright">' . $args['copyright'] . '</span>';
+        }
+
         $html .= '</div>';
 
         return $html;
