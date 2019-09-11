02b286b41cb70cccfa029230b60656515bfa1f0c
Dennis Krohn <dennis.krohn@th-luebeck.de>
2019-09-11 18:06:14 +0200

    added various external content services

--- includes/LoopExternalContent.php
+++ includes/LoopExternalContent.php
@@ -20,6 +20,10 @@ class LoopExternalContent {
 	 */
 	public static function onParserSetup( Parser $parser ) {
 		$parser->setHook ( 'h5p', 'LoopExternalContent::renderH5P' );
+		$parser->setHook ( 'learningapp', 'LoopExternalContent::renderLearningApp' );
+		$parser->setHook ( 'padlet', 'LoopExternalContent::renderPadlet' );
+		$parser->setHook ( 'prezi', 'LoopExternalContent::renderPrezi' );
+		$parser->setHook ( 'slideshare', 'LoopExternalContent::renderSlideshare' );
 		return true;
     }	
     
@@ -53,9 +57,8 @@ class LoopExternalContent {
                     'src' => $hostUrl . $id,
                     'width' => $width,
                     'height' => $height,
-                    'frameborder' => '0',
                     'allowfullscreen' => 'allowfullscreen',
-                    'class' => 'responsive-iframe' # TODO RESPONSIVE
+                    'class' => 'ext-h5p responsive-iframe'
                 ),
                 ''
             );
@@ -72,5 +75,177 @@ class LoopExternalContent {
     }
 
     
+    public static function renderLearningApp ( $input, array $args, Parser $parser, PPFrame $frame ) { 
+
+        global $wgLearningAppUrl;
+        $errors = '';
+        $return = '';
+        $appId = '';
+        $width = array_key_exists( 'width', $args ) ? $args['width'] : '700';
+        $height = array_key_exists( 'height', $args ) ? $args['height'] : '500';
+        $hostUrl = $wgLearningAppUrl;
+
+        if ( array_key_exists( 'app', $args ) ) {
+            $appId = "app=" . $args["app"];
+        } elseif ( array_key_exists( 'privateapp', $args ) ) {
+            $appId = "v=" . $args["privateapp"];
+        } else {
+            $errors .= wfMessage('loopexternalcontent-learningapp-error-noid')->text() . "<br>";
+        }
+
+        if ( !empty( $appId ) ) {
+            $return = Html::rawElement(
+                'iframe',
+                array(
+                    'src' => $hostUrl . $appId,
+                    'width' => $width,
+                    'height' => $height,
+                    'allowfullscreen' => 'allowfullscreen',
+                    'class' => 'ext-learningapp responsive-iframe'
+                ),
+                ''
+            );
+        } 
+        
+        if ( !empty ( $errors ) ) {
+            $return .= new LoopException( $errors );
+            $parser->addTrackingCategory( 'loop-tracking-category-error' );
+			
+        }
+
+        return $return;
+    }
+
+    public static function renderPadlet ( $input, array $args, Parser $parser, PPFrame $frame ) { 
+
+        global $wgPadletUrl;
+        $errors = '';
+        $return = '';
+        $key = '';
+        $width = array_key_exists( 'width', $args ) ? $args['width'] : '700';
+        $height = array_key_exists( 'height', $args ) ? $args['height'] : '500';
+        $hostUrl = $wgPadletUrl;
+
+        if ( array_key_exists( 'key', $args ) ) {
+            $key = $args["key"];
+        } elseif ( array_key_exists( 'id', $args ) ) {
+            $key = $args["id"];
+        } else {
+            $errors .= wfMessage('loopexternalcontent-padlet-error-noid')->text() . "<br>";
+        }
+
+        if ( !empty( $key ) ) {
+            $return = Html::rawElement(
+                'iframe',
+                array(
+                    'src' => $hostUrl . $key,
+                    'width' => $width,
+                    'height' => $height,
+                    'allowfullscreen' => 'allowfullscreen',
+                    'class' => 'ext-padlet responsive-iframe'
+                ),
+                ''
+            );
+        } 
+        
+        if ( !empty ( $errors ) ) {
+            $return .= new LoopException( $errors );
+            $parser->addTrackingCategory( 'loop-tracking-category-error' );
+			
+        }
+
+        return $return;
+    }
+    
+    public static function renderPrezi ( $input, array $args, Parser $parser, PPFrame $frame ) { 
+
+        global $wgPreziUrl;
+        $errors = '';
+        $return = '';
+        $id = '';
+        $width = array_key_exists( 'width', $args ) ? $args['width'] : '550';
+        $height = array_key_exists( 'height', $args ) ? $args['height'] : '500';
+        $controls = ( array_key_exists( 'control', $args ) && $args["control"] == strtolower("simple") ) ? '1' : '0';
+        $title = array_key_exists( 'title', $args ) ? $args['title'] : '';
+        $hostUrl = $wgPadletUrl;
+
+        if ( array_key_exists( 'id', $args ) ) {
+            $id = $args["id"];
+        } else {
+            $errors .= wfMessage('loopexternalcontent-prezi-error-noid')->text() . "<br>";
+        }
+
+        if ( !empty( $id ) ) {
+            $return = Html::openElement( 'div', array( 'class' => 'prezi-player', '') );
+            $return .= Html::rawElement(
+                'iframe',
+                array(
+                    'src' => $hostUrl . $id . '/?bgcolor=ffffff&amp;lock_to_path='.$controls.'&amp;autoplay=0&amp;autohide_ctrls=0',
+                    'width' => $width,
+                    'height' => $height,
+                    'allowfullscreen' => 'allowfullscreen',
+                    'class' => 'ext-prezi responsive-iframe'
+                ),
+                ''
+            );
+            $return = Html::rawElement( 
+                'div', 
+                array( 
+                    'class' => 'prezi-player-links text-center' 
+                ), 
+                '<p><a class="external-link" target="_blank" href="https://prezi.com/'.$id.'/">'.$title.'</a> '.wfMessage( "loopexternalcontent-prezi-on" )->text().' <a class="external-link" target="_blank" href="https://prezi.com">Prezi</a></p>'
+            );
+            $return = Html::closeElement( 'div' );
+        } 
+        
+        if ( !empty ( $errors ) ) {
+            $return .= new LoopException( $errors );
+            $parser->addTrackingCategory( 'loop-tracking-category-error' );
+			
+        }
+
+        return $return;
+    }
+    
+    public static function renderSlideshare ( $input, array $args, Parser $parser, PPFrame $frame ) { 
+
+        global $wgSlideshareUrl;
+        $errors = '';
+        $return = '';
+        $key = '';
+        $width = array_key_exists( 'width', $args ) ? $args['width'] : '700';
+        $height = array_key_exists( 'height', $args ) ? $args['height'] : '500';
+        $hostUrl = $wgSlideshareUrl;
+
+        if ( array_key_exists( 'key', $args ) ) {
+            $key = $args["key"];
+        } elseif ( array_key_exists( 'id', $args ) ) {
+            $key = $args["id"];
+        } else {
+            $errors .= wfMessage('loopexternalcontent-slideshare-error-noid')->text() . "<br>";
+        }
+
+        if ( !empty( $key ) ) {
+            $return = Html::rawElement(
+                'iframe',
+                array(
+                    'src' => $hostUrl . $key,
+                    'width' => $width,
+                    'height' => $height,
+                    'allowfullscreen' => 'allowfullscreen',
+                    'class' => 'ext-slideshare responsive-iframe'
+                ),
+                ''
+            );
+        } 
+        
+        if ( !empty ( $errors ) ) {
+            $return .= new LoopException( $errors );
+            $parser->addTrackingCategory( 'loop-tracking-category-error' );
+			
+        }
+
+        return $return;
+    }
 
 }
