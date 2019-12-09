c2e33d2372dd972395056a04c2ebe35d067a6cf5
duness <dness@dness.de>
2019-12-09 10:31:12 +0100

    working, no css tuning

--- includes/LoopZoom.php
+++ includes/LoopZoom.php
@@ -21,7 +21,7 @@ class LoopZoom {
         $html .= $parser->recursiveTagParseFully( $input );
         $html .= '</div>';
 
-        $html .= '<div class="modal fade loopzoom-'.$unique_id.'-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        $html .= '<div class="modal fade loopzoom-' . $unique_id . '-modal" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog modal-lg">
             <div class="modal-content"></div>
         </div>
@@ -29,17 +29,4 @@ class LoopZoom {
 
         return $html;
     }
-
 }
-/*
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Large modal</button>
-
-<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
-    <div class="modal-content">
-      ...
-    </div>
-  </div>
-</div>
-
-*/
