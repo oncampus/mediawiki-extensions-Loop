db03a99a05684c52153d5656d803d2404a5e67e3
nessdgit config --global user.name duness <education@oncampus.de>
2020-02-24 14:12:45 +0100

    cache problem fixed

--- includes/LoopConsent.php
+++ includes/LoopConsent.php
@@ -4,55 +4,45 @@
   * @author Dustin Neß <dustin.ness@th-luebeck.de>
   */
 
-
- //include $_SERVER['DOCUMENT_ROOT'].'/extensions/EmbedVideo/EmbedVideo.hooks.php';
+  /*
+  TODOS:
+  - i18n für ES und SV
+  - ggf. Support ausweiten neben <youtube>-Tag
+  */
 
 if( !defined( 'MEDIAWIKI' ) ) { die( "This file cannot be run standalone.\n" ); }
 
 class LoopConsent {
-	public static function onParserBeforeStrip( &$parser ) {
-    global $wgOut;
-
-    if(!isset($_COOKIE['loopYtConsent'])) {
-       $parser->setHook( 'ev', 'LoopConsent::handleConsentPrompt' );
-       $parser->setHook( 'youtube', 'LoopConsent::handleConsentPrompt' );
-      }
-    
-      // $setting1= (int) $parser->getUser()->getOption('loopYtConsent');
-      // $parser->getOptions()->optionUsed( 'loopYtConsent' );
-
-      
-		$user = $wgOut->getUser();
-		$consent = $user->getOption( 'loopYtConsent', false, true );
-    $parser->getOptions()->optionUsed( 'loopYtConsent' );
-    
-
+    public static function onParserBeforeStrip( &$parser ) {
+        if( !isset( $_COOKIE['LoopYtConsent'] ) ) {
+            $parser->setHook( 'youtube', 'LoopConsent::handleConsentPrompt' );
+        }
     }
-    
+
     public static function handleConsentPrompt( $input, array $args, Parser $parser, PPFrame $frame ) {
-      if (preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/\s]{11})%i', $input, $match)) {
-        $video_id = $match[1];
-      }
+        if ( preg_match( '%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/\s]{11})%i', $input, $match ) ) {
+            $video_id = $match[1];
+        }
         
         $out = '<div class="loop_consent" style="background-image: url(https://img.youtube.com/vi/'. $video_id .'/maxresdefault.jpg)">';
-        $out .= '<div class="loop_consent_text"><p>Wenn Sie das Video starten, werden Inhalte von YouTube geladen und dadurch Ihre IP-Adresse an YouTube übertragen.</p>';
-        $out .= '<button class="btn btn-dark btn-block border-0 loop_consent_agree">⯈ Video ansehen und nicht erneut fragen</button>';
+        $out .= '<div class="loop_consent_text"><p>' . wfMessage('loopconsent-text') . '</p>';
+        $out .= '<button class="btn btn-dark btn-block border-0 loop_consent_agree">⯈ ' . wfMessage('loopconsent-button') . '</button>';
         $out .= '</div></div>';
         
         return $out;
     }
 
-    public static function onPageRenderingHash( &$confstr, $user, $optionsUsed ) {
-    
-      global $wgDefaultUserOptions;
+    public static function onPageRenderingHash( &$confstr, $user, &$optionsUsed ) {
+        $loopYtConsentVal  = $user->getOption( 'LoopYtConsent' );
+      
+        if( isset( $loopYtConsentVal ) && ( in_array( $loopYtConsentVal, array( "0", "1" ) ) ) ) {
+            if( isset( $_COOKIE['LoopYtConsent'] ) ) {
+                $confstr .= "!LoopYtConsent=true";
+            } else {
+                $confstr .= "!LoopYtConsent=false";
+            }
+        }
 
-      $consent = $user->getOption( 'loopYtConsent', false, true );
-  
-      if ( $consent ) {
-        $confstr .= "!loopYtConsent=true";
-      } else {
-        $confstr .= "!loopYtConsent=false";
-      }
-
+      return true;
     }
 }
