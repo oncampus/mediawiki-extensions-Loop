93c9c5a3e783ca507ce24859cd0ed1ee5459ac5d
nessdgit config --global user.name duness <education@oncampus.de>
2020-02-24 09:19:01 +0100

    works, still cache problems

--- includes/LoopConsent.php
+++ includes/LoopConsent.php
@@ -11,23 +11,48 @@ if( !defined( 'MEDIAWIKI' ) ) { die( "This file cannot be run standalone.\n" ); 
 
 class LoopConsent {
 	public static function onParserBeforeStrip( &$parser ) {
-  //dd($_COOKIE);
+    global $wgOut;
+
     if(!isset($_COOKIE['loopYtConsent'])) {
        $parser->setHook( 'ev', 'LoopConsent::handleConsentPrompt' );
        $parser->setHook( 'youtube', 'LoopConsent::handleConsentPrompt' );
       }
-        // $parser->setFunctionHook( "ev", "EmbedVideoHooks::parseEV" );
-    // return true;
-   // dd('asd');
+    
+      // $setting1= (int) $parser->getUser()->getOption('loopYtConsent');
+      // $parser->getOptions()->optionUsed( 'loopYtConsent' );
+
+      
+		$user = $wgOut->getUser();
+		$consent = $user->getOption( 'loopYtConsent', false, true );
+    $parser->getOptions()->optionUsed( 'loopYtConsent' );
+    
+
     }
     
     public static function handleConsentPrompt( $input, array $args, Parser $parser, PPFrame $frame ) {
-       // dd($parser);
-        $out = '<div class="loop_consent">';
-        $out .= '<p>Wenn Sie das Video starten, werden Inhalte von YouTube geladen und dadurch Ihre IP-Adresse an YouTube übertragen.</p>';
+      if (preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/\s]{11})%i', $input, $match)) {
+        $video_id = $match[1];
+      }
+        
+        $out = '<div class="loop_consent" style="background-image: url(https://img.youtube.com/vi/'. $video_id .'/maxresdefault.jpg)">';
+        $out .= '<div class="loop_consent_text"><p>Wenn Sie das Video starten, werden Inhalte von YouTube geladen und dadurch Ihre IP-Adresse an YouTube übertragen.</p>';
         $out .= '<button class="btn btn-dark btn-block border-0 loop_consent_agree">⯈ Video ansehen und nicht erneut fragen</button>';
-        $out .= '</div>';
+        $out .= '</div></div>';
         
         return $out;
+    }
+
+    public static function onPageRenderingHash( &$confstr, $user, $optionsUsed ) {
+    
+      global $wgDefaultUserOptions;
+
+      $consent = $user->getOption( 'loopYtConsent', false, true );
+  
+      if ( $consent ) {
+        $confstr .= "!loopYtConsent=true";
+      } else {
+        $confstr .= "!loopYtConsent=false";
+      }
+
     }
 }
