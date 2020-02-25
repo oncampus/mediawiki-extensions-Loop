7307c17193807607887a9d362429271c181b0337
nessdgit config --global user.name duness <education@oncampus.de>
2020-02-25 10:18:31 +0100

    zwischenstand

--- includes/LoopConsent.php
+++ includes/LoopConsent.php
@@ -7,7 +7,6 @@
   /*
   TODOS:
   - i18n für ES und SV
-  - ggf. Support ausweiten neben <youtube>-Tag
   */
 
 if( !defined( 'MEDIAWIKI' ) ) { die( "This file cannot be run standalone.\n" ); }
@@ -15,26 +14,60 @@ if( !defined( 'MEDIAWIKI' ) ) { die( "This file cannot be run standalone.\n" ); 
 class LoopConsent {
     public static function onParserBeforeStrip( &$parser ) {
         if( !isset( $_COOKIE['LoopYtConsent'] ) ) {
-            $parser->setHook( 'youtube', 'LoopConsent::ParseYoutube' );     // <youtube>
-            $parser->setHook( 'embedvideo', 'LoopConsent::ParseYoutube' );  // <embedvideo> 
+            $parser->setHook( 'youtube', 'LoopConsent::parseYoutube' );     // <youtube>
+            $parser->setHook( 'embedvideo', 'LoopConsent::parseYoutube' );  // <embedvideo>
+            $parser->setFunctionHook( 'ev', 'LoopConsent::parseEv' );       // {{#ev}}
+            $parser->setFunctionHook( 'evt', 'LoopConsent::parseEv' );       // {{#evt}}
+            $parser->setFunctionHook( 'evu', 'LoopConsent::parseEvu' );       // {{#evu}}
         }
     }
 
 
-    public static function ParseYoutube( $input, array $args, Parser $parser, PPFrame $frame ) {
-        if ( preg_match( '%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/\s]{11})%i', $input, $match ) ) {
-            $video_id = $match[1];
-        }
-        
+    public static function parseYoutube( $input, array $args, Parser $parser, PPFrame $frame ) {
         $lc = new LoopConsent();
-        return $lc->renderOutput($video_id);
+
+        return $lc->renderOutput( $lc->getYouTubeId( $input ) );
     }
 
 
-    public static function ParseEmbedVideo( $input, array $args, Parser $parser, PPFrame $frame) {
-        //dd($input);
-        $out = '';
+    public static function parseEv( $parser, $callback, $flags) {
+        $lc = new LoopConsent();
+
+        return [
+            $lc->renderOutput( $lc->getYouTubeId( $flags ) ),
+            'noparse'=> true,
+            'isHTML' => true
+        ];
+    }
+
+
+    public static function parseEvu( $parser, $callback, $flags ) {
+        $lc = new LoopConsent();
+
+        return [
+            $lc->renderOutput( $lc->getYouTubeId( $callback ) ),
+            'noparse'=> true,
+            'isHTML' => true
+        ];
+    }
+
+    
+    private function renderOutput( $id ) {
+        $out = '<div class="loop_consent" style="background-image: url(https://img.youtube.com/vi/'. $id .'/maxresdefault.jpg)">';
+        $out .= '<div class="loop_consent_text"><p>' . wfMessage('loopconsent-text') . '</p>';
+        $out .= '<button class="btn btn-dark btn-block border-0 loop_consent_agree">⯈ ' . wfMessage('loopconsent-button') . '</button>';
+        $out .= '</div></div>';
+ 
         return $out;
+    }
+
+
+    private function getYouTubeId( $url ) {
+        if ( preg_match( '%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/\s]{11})%i', $url, $match ) ) {
+            return $match[1];
+        }
+
+        return false;
     }
 
 
@@ -50,15 +83,5 @@ class LoopConsent {
         }
 
       return true;
-    }
-
-
-    private function renderOutput( $id ) {
-        $out = '<div class="loop_consent" style="background-image: url(https://img.youtube.com/vi/'. $id .'/maxresdefault.jpg)">';
-        $out .= '<div class="loop_consent_text"><p>' . wfMessage('loopconsent-text') . '</p>';
-        $out .= '<button class="btn btn-dark btn-block border-0 loop_consent_agree">⯈ ' . wfMessage('loopconsent-button') . '</button>';
-        $out .= '</div></div>';
-        
-        return $out;
     }
 }
