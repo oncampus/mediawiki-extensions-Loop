7480ae20b20a3580a474407380900360f4cde8af
Dennis Krohn <dennis.krohn@th-luebeck.de>
2020-04-27 16:12:04 +0200

    fixed spoiler math

--- resources/js/loop.spoiler.js
+++ resources/js/loop.spoiler.js
@@ -5,22 +5,28 @@
 
 $('.loopspoiler').click(function() {
   
-  if($('.loopspoiler_default_content').length) {
-    $('.loopspoiler_default_content').remove();
-    return 0;
-  } else if($('.loopspoiler_transparent_content').length) {
-    $('.loopspoiler_transparent_content').remove();
-    return 0;
-  }
-
-  let content = $(this).attr('data-spoilercontent');
-
-  if($(this).hasClass('loopspoiler_type_transparent')) {
-    type = 'transparent';
+  if ($(this).attr("data-loaded") == "true") {
+    if($('.loopspoiler_default_content').length) {
+      $('.loopspoiler_default_content').toggle();
+    } else if($('.loopspoiler_transparent_content').length) {
+      $('.loopspoiler_transparent_content').toggle();
+    }
   } else {
-    type = 'default';
+    let content = $(this).attr('data-spoilercontent');
+
+    if($(this).hasClass('loopspoiler_type_transparent')) {
+      type = 'transparent';
+    } else {
+      type = 'default';
+    }
+    
+    $('#' + $(this).attr("id") +  " .mwe-math-element").each(function() {
+      content = content.replace('<span class="loopspoiler_math_replace"></span>', $(this).html() );
+      $(this).remove();
+    })
+    $('#' + $(this).attr("id")).attr("data-loaded", true);
+    $(this).after('<div class="loopspoiler_' + type + '_content">' + content + '</div>')
+  
   }
-
-  $(this).after('<div class="loopspoiler_' + type + '_content">' + content + '</div>')
-
+  
 });
