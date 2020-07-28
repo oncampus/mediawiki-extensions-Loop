ed632448bfa7ed3cd82a085ae315a1ed24daf9b5
Dennis Krohn <dennis.krohn@th-luebeck.de>
2020-07-28 16:50:46 +0200

    re-added print js module for backwards compatibility

---
+++ resources/loop.printtag.js
@@ -0,0 +1,17 @@
+/**
+  * @description Script for <loop_print> tag (includes/LoopPrint.php)
+  * @author Dustin Neß <dustin.ness@th-luebeck.de>
+  */
+ $('.loopprint-tag').click(function() {
+    if($(this).parent('.loopprint-button').length <= 0) return;
+
+    let getId = $(this).attr('class').split(' ')[1];
+    let btnIcon = '<span class="ic ic-print-area float-none"></span>';
+    let btnText = btnIcon + $(this).attr('data-title');
+
+    $('#' + getId).toggle();
+    $(this).toggleClass('printbuttonactive');
+    $(this).html($(this).html() == btnText ? btnIcon : btnText);
+
+    return false;
+});
