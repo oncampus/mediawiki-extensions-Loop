c9ef9607f4ce5d757c8e25f6af63782ec4b9ad7e
krohnden <dennis.krohn@th-luebeck.de>
2021-09-29 16:38:44 +0200

    re-added stripstate

--- includes/LoopObject.php
+++ includes/LoopObject.php
@@ -395,7 +395,7 @@ class LoopObject {
 			$t = Title::newFromText ( 'NO TITLE' );
 			$parser->setTitle ( $t );
 			$parser->clearState ();
-			# $parser->mStripState = new StripState( $parser ); #safe to remove?
+			$parser->mStripState = new StripState( $parser );
 			$frame = $parser->getPreprocessor ()->newFrame ();
 		}
 
