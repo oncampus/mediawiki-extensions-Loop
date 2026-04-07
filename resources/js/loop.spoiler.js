247ad2f55bbe827017c79b426d373ff6b3f6e94c
Daniel Lembcke <daniel.lembcke@oncampus.de>
2026-04-07 10:43:51 +0200

    Call mathjax in loop spoiler logic and set opacity for formulas in the loop spoiler to 1

--- resources/js/loop.spoiler.js
+++ resources/js/loop.spoiler.js
@@ -4,44 +4,83 @@
   * @author Dennis Krohn <dustin.ness@th-luebeck.de>
   */
 
-$('.loopspoiler').click(function() {
-  let id = $(this).attr("id");
-  $("html").css("background-color", $("#main-footer").css("background-color"));
-  if ($(this).attr("data-loaded") == "true") {
-    if($('.loopspoiler_default_content.'+id).length) {
-      $('.loopspoiler_default_content.'+id).toggle();
-    } else if($('.loopspoiler_transparent_content.'+id).length) {
-      $('.loopspoiler_transparent_content.'+id).toggle();
-    }
-  } else {
-    let content = $(this).attr('data-spoilercontent');
+$('.loopspoiler').click(function () {
+	let id = $(this).attr("id");
+	let $trigger = $(this);
 
-    if($(this).hasClass('loopspoiler_type_transparent')) {
-      type = 'transparent';
-    } else {
-      type = 'default';
-    }
-    
-    let regexp = /data-marker="(\w{8})"/g;
-    let matches = content.matchAll(regexp);
-    let contentids = new Array;
-    let mathids = new Array;
+	$("html").css("background-color", $("#main-footer").css("background-color"));
 
-    for ( let match of matches ) {
-      contentids.push(match[1]);
-    }
-    contentids.sort();
+	function typesetIfNeeded($element) {
+		if (
+			typeof window.MathJax !== 'undefined' &&
+			typeof MathJax.typesetPromise === 'function'
+		) {
+			requestAnimationFrame(() => {
+				MathJax.typesetPromise([$element[0]]).catch((err) => {
+					console.error('MathJax typeset failed:', err);
+				});
+			});
+		}
+	}
 
-    $('#' + $(this).attr("id") +  " .replacemath").each(function() {
-      mathids.push($(this).attr("id"));
-    })
-    for ( let i = 0; i < mathids.length; i++ ) {
-      content = content.replace('<span class="loopspoiler_math_replace" data-marker="'+contentids[i]+'"></span>', $('#' + $(this).attr("id") +  ' #' + mathids[i] + " .mwe-math-element").html() );
-      $('#' + $(this).attr("id") + " .replacemath #"+mathids[i] + " .mwe-math-element").remove();
-    }
-    $('#' + $(this).attr("id")).attr("data-loaded", true);
-    $(this).after('<div class="loopspoiler_' + type + '_content ' + id + '">' + content + '</div>')
-    
-  }
-  
+	if ($trigger.attr("data-loaded") == "true") {
+		let $content;
+
+		if ($('.loopspoiler_default_content.' + id).length) {
+			$content = $('.loopspoiler_default_content.' + id);
+		} else if ($('.loopspoiler_transparent_content.' + id).length) {
+			$content = $('.loopspoiler_transparent_content.' + id);
+		}
+
+		if ($content && $content.length) {
+			$content.toggle();
+
+			if ($content.is(':visible')) {
+				$content.find('.smj-container').css('opacity', '1');
+				typesetIfNeeded($content);
+			}
+		}
+	} else {
+		let content = $trigger.attr('data-spoilercontent');
+		let type;
+
+		if ($trigger.hasClass('loopspoiler_type_transparent')) {
+			type = 'transparent';
+		} else {
+			type = 'default';
+		}
+
+		let regexp = /data-marker="(\w{8})"/g;
+		let matches = content.matchAll(regexp);
+		let contentids = [];
+		let mathids = [];
+
+		for (let match of matches) {
+			contentids.push(match[1]);
+		}
+		contentids.sort();
+
+		$('#' + id + " .replacemath").each(function () {
+			mathids.push($(this).attr("id"));
+		});
+
+		for (let i = 0; i < mathids.length; i++) {
+			content = content.replace(
+				'<span class="loopspoiler_math_replace" data-marker="' + contentids[i] + '"></span>',
+				$('#' + id + ' #' + mathids[i] + " .mwe-math-element").html()
+			);
+			$('#' + id + " .replacemath #" + mathids[i] + " .mwe-math-element").remove();
+		}
+
+		content = content.replace(/opacity\s*:\s*(?:\.5|0\.5)/g, 'opacity:1');
+
+		$trigger.attr("data-loaded", true);
+
+		let $newContent = $('<div class="loopspoiler_' + type + '_content ' + id + '">' + content + '</div>');
+		$newContent.find('.smj-container').css('opacity', '1');
+		$trigger.after($newContent);
+
+		typesetIfNeeded($newContent);
+	}
+
 });
