3d06fe20d5b7d4c60da5f9af2bbe72cd27eccee2
Jakob Giertz <jakob.giertz@oncampus.de>
2026-05-13 17:18:52 +0200

    minor fixes, only typing

--- includes/LoopNoSpeech.php
+++ includes/LoopNoSpeech.php
@@ -8,16 +8,18 @@
 if ( !defined( 'MEDIAWIKI' ) ) die ( "This file cannot be run standalone.\n" );
 
 class LoopNoSpeech {
-	var $input='';
-	var $args=array();
+	var string $input='';
+	var array $args=array();
 
-	public static function onParserSetup( Parser $parser) {
+	public static function onParserSetup( Parser $parser): bool
+	{
 		$parser->setHook( 'loop_nospeech', 'LoopNoSpeech::renderLoopNoSpeech' );
 
 		return true;
 	}
 
-	static function renderLoopNoSpeech( $input, array $args, Parser $parser, PPFrame $frame ) {
+	static function renderLoopNoSpeech( $input, array $args, Parser $parser, PPFrame $frame ): string
+	{
 		$return='';
 		$nospeech_id=uniqid();
 		$return.='<div class="nospeecharea" id="'.$nospeech_id.'">';
